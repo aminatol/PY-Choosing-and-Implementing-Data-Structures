435cd09603341f78f70bb66b84291b03e05211c2
aminatol <aminatoluwo@gmail.com>
2023-10-14 21:04:30 -0400

    first commit

---
+++ _practice.py
@@ -0,0 +1,14 @@
+print('hello World')
+
+
+def Hello():
+    return ''
+Hello()
+
+def pack (first, second, third):
+    print('first, second, third')
+pack('One', 'Two', 'Three')
+
+def eat_lunch(one, two ,three):
+    print (f"first I eat {one}, then I ate {two}, last I ate {three}")
+eat_lunch("avacado toast", "salad", "ramen") 
